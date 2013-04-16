3cf30019b389da95983cf6d987b46dd67e9ee958
Chris Keating <christopherkeating@gmail.com>
2013-04-16 01:19:00 -0500

    tweaks

--- project.clj
+++ project.clj
@@ -2,21 +2,22 @@
   :source-paths ["src-clj"]
   :depedencies [[org.clojure/clojure "1.4.0"]
                 [compojure "1.0.4"]
-                [hiccup "1.0.0"]]
+                [hiccup "1.0.3"]]
   :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-ring "1.0.0"]]
+            [lein-ring "0.7.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {
-    :repl-listen-port 9000
-    :repl-launch-commands {
-      "safari" ["safari"
-                :stdout ".repl-safari-out"
-                :stderr ".repl-safari-err"]}
+    :builds [],
     :build {
       :dev
         {:source-paths ["src-cljs"]
          :jar true
-         :compiler {:output-to "resources/public/js/main-debug.js"
+         :compiler {:output-to "resources/public/js/main.js"
                     :optimizations :whitespace
-                    :pretty-print true}}}}
+                    :pretty-print true}}
+    :repl-listen-port 9000
+    :repl-launch-commands {
+      "safari" ["safari"
+                :stdout ".repl-safari-out"
+                :stderr ".repl-safari-err"]}}}
   :ring {:handler tableau.routes/app})
