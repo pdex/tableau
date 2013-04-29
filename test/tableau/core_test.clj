b80b85bd14fb1828e84c2fe8fb64d119bb607d35
Chris Keating <christopherkeating@gmail.com>
2013-04-29 08:36:10 -0500

    initial tests

---
+++ test/tableau/core_test.clj
@@ -0,0 +1,41 @@
+(ns tableau.core-test
+  (:require [clojure.test :refer :all]
+            [tableau.core :refer :all]))
+
+(def ^:dynamic cards [])
+
+(def ^:dynamic players [])
+
+(def ^:dynamic hands [])
+
+(defn setup-cards [f]
+  (binding [cards [:ace :king :queen :jack]]
+  (f)))
+
+(defn setup-players [f]
+  (binding [players [:player1 :player2]
+            hands {:player1 [] :player2 []}]
+  (f)))
+
+(use-fixtures
+  :each setup-cards setup-players)
+
+(deftest test-dealing
+  (testing "Test dealing 1 card to each player"
+    (let [hands (map () cards)]))
+  (println cards)
+  (println players)
+  (println hands)
+  )
+
+(deftest test-drawing
+  (testing "Test drawing 1 card"
+    (let [card (draw 1 cards)]
+      (is (= card [:ace]))))
+  (testing "Test drawing 2 cards"
+    (let [card (draw 2 cards)]
+      (is (= card [:ace :king])))))
+
+(deftest test-shuffling
+  (testing "Test shuffling")
+  )
